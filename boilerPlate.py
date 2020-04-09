3d49983825a4f1d7a1ba7749c2872c141831365a
rajeshwar-sopho <rajeshwarrn98@gmail.com>
2020-04-10 02:49:11 +0530

    changed files.

--- boilerPlate.py
+++ boilerPlate.py
@@ -5,6 +5,11 @@ t = int(input())
 
 for _ in range(t):
 	n = int(input())
-	arr = list(map(int, input().split()))
-
-	ans = fun(arr, n)
+	
+        # for integer input array
+        arr = list(map(int, input().split()))
+        
+        # for string input array
+        arr = input().split()
+	
+        ans = fun(arr, n)
