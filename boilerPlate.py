94e634585ffdfa81eecfd9a789ff107e0f73bbd6
rajeshwar-sopho <rajeshwarrn98@gmail.com>
2020-04-11 19:44:39 +0530

    added a __str__ function.

--- boilerPlate.py
+++ boilerPlate.py
@@ -1,12 +1,19 @@
 class Mat:
 	def __init__(self, arr, m, n):
 		# there are m rows and n columns
+		self.dim = (m, n)
 		self.mat = []
 		for i in range(m):
 			temp = []
 			for j in range(n):
 				temp.append(arr[i*n + j])
 			self.mat.append(temp)
+
+	def __str__(self):
+		for i in range(self.dim[0]):
+			for j in range(self.dim[1]):
+				print(self.mat[i][j], end=' ')
+
 
 
 def fun(arr, n):
